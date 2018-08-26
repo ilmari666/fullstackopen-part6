fbd74c0ea7ba86d7e5e1734e6d9a10eda2f2e672
Ilmari <ilmari@sokeri.com>
2018-08-26 23:00:21 +0300

    update footer text

--- routed-anecdotes/src/components/footer.js
+++ routed-anecdotes/src/components/footer.js
@@ -4,11 +4,11 @@ const Footer = () => (
   <div>
     Anecdote app for{' '}
     <a href="https://courses.helsinki.fi/fi/TKT21009/121540749">
-      Full Stack -sovelluskehitys
+      Full Stack -sovelluskehitys harjoitustyö
     </a>
     . See{' '}
-    <a href="https://github.com/mluukkai/routed-anecdotes">
-      https://github.com/mluukkai/routed-anecdotes
+    <a href="https://github.com/ilmari666/fullstackopen-part6">
+      https://github.com/ilmari666/fullstackopen-part6
     </a>{' '}
     for the source code.
   </div>
